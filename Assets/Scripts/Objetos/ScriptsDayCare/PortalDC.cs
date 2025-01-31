a44dab9f7d9669609a8616a37539e499b30c64f4
Adriiaruizz <adriiaruizz@gmail.com>
2025-01-31 13:44:38 +0100

    Finalizado Mapa DayCare
    -Se ha acabado el mapeado de DayCare
    -Falta añadir funcionalidad

---
+++ Assets/Scripts/Objetos/ScriptsDayCare/PortalDC.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; // Necesario para cambiar escenas
+
+public class PortalDC : MonoBehaviour
+{
+    public string nextSceneName; // Nombre de la escena a cargar
+    public string interactableTag = "Player"; // Solo el jugador puede activar el portal
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Verificar si el objeto que entra tiene el tag correcto
+        if (other.CompareTag(interactableTag))
+        {
+            Debug.Log("El jugador ha entrado al portal.");
+
+            // Activar el booleano en GlobalGameState
+            GlobalGameState.DaycareCompleta = true;
+            Debug.Log("GlobalGameState: DressingRoomCompleta activado.");
+
+            // Cambiar de escena
+            ChangeScene();
+        }
+    }
+
+    void ChangeScene()
+    {
+        // Verificar si el nombre de la escena está configurado
+        if (!string.IsNullOrEmpty(nextSceneName))
+        {
+            SceneManager.LoadScene(nextSceneName);
+        }
+        else
+        {
+            Debug.LogError("El nombre de la siguiente escena no está configurado.");
+        }
+    }
+}
